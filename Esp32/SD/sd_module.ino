8fdc43811985bb8075029a21cb85e1db50f68767
Aquila <reenacottage@gmail.com>
2024-02-17 19:20:36 +0530

    formatting

--- Esp32/SD/sd_module.ino
+++ Esp32/SD/sd_module.ino
@@ -1,66 +1,50 @@
 #include "SD_MMC.h"
 
-void clearAll()
-{
-    if (SD_MMC.remove("/samples/"))
-    {
-        Serial.println("samples deleted cleared");
-    }
-    else
-    {
-        Serial.println("samples deletion failed");
-    }
+void clearAll() {
+  if (SD_MMC.remove("/samples/")) {
+    Serial.println("samples deleted cleared");
+  } else {
+    Serial.println("samples deletion failed");
+  }
 }
 
-void writeSample(uint16_t *data, String filename)
-{
-    File file = SD_MMC.open(filename, FILE_WRITE);
-    if (!file)
-    {
-        Serial.println("File create failed");
-        return;
-    }
-    file.write(*data);
-    file.close();
-    Serial.println("File " + filename + " write successful");
+void writeSample(uint16_t *data, String filename) {
+  File file = SD_MMC.open(filename, FILE_WRITE);
+  if (!file) {
+    Serial.println("File create failed");
+    return;
+  }
+  file.write(*data);
+  file.close();
+  Serial.println("File " + filename + " write successful");
 }
 
-void setupSD()
-{
-    if (!SD_MMC.begin())
-    {
-        Serial.println("SD Card initialization failed!");
-        return;
-    }
+void setupSD() {
+  if (!SD_MMC.begin()) {
+    Serial.println("SD Card initialization failed!");
+    return;
+  }
 
-    if (SD_MMC.exists("/samples/"))
-    {
-        Serial.println("folder exists");
+  if (SD_MMC.exists("/samples/")) {
+    Serial.println("folder exists");
+  } else {
+    Serial.println("File does not exist");
+    if (SD_MMC.mkdir("/samples/")) {
+      Serial.println("samples directory created");
+    } else {
+      Serial.println("samples directory creation failed");
     }
-    else
-    {
-        Serial.println("File does not exist");
-        if (SD_MMC.mkdir("/samples/"))
-        {
-            Serial.println("samples directory created");
-        }
-        else
-        {
-            Serial.println("samples directory creation failed");
-        }
-    }
+  }
 }
 
-void setup()
-{
-    Serial.begin(9600);
-    setupSD();
-    const char *payload = "hey this is bulletlaunch";
-    uint16_t number = (uint16_t)strtoul(payload, NULL, 10);
-    writeSample(&number, "sample.txt");
+void setup() {
+  Serial.begin(9600);
+  setupSD();
+  const char *payload = "hey this is bulletlaunch";
+  uint16_t number = (uint16_t)strtoul(payload, NULL, 10);
+  writeSample(&number, "sample.txt");
 }
 
-void loop()
-{
-    // put your main code here, to run repeatedly:
+void loop() {
+  // put your main code here, to run repeatedly:
 }
