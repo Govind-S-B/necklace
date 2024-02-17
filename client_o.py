4dd45ccfa3fb54f0315e6df061c78ea40eabb735
Govind-S-B <b.s.dnivog@gmail.com>
2024-02-17 11:05:30 +0530

    minimal

---
+++ client_o.py
@@ -0,0 +1,13 @@
+import asyncio
+import websockets
+
+SERVER_ADDR = "192.168.28.180"
+
+async def client_handler():
+    async with websockets.connect(f"ws://{SERVER_ADDR}:8765") as websocket:
+        while True:
+            pass
+            # message = await websocket.recv()
+            # print(f"Received: {message}")
+
+asyncio.get_event_loop().run_until_complete(client_handler())
